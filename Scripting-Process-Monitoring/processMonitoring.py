8eaba2277f1e9ce55090315fad723980b9f77f4d
Dor Bitton <53118239+DorBitton@users.noreply.github.com>
2023-02-07 02:37:13 +0200

    Update processMonitoring.py

--- Scripting-Process-Monitoring/processMonitoring.py
+++ Scripting-Process-Monitoring/processMonitoring.py
@@ -3,6 +3,14 @@ import os
 import time
 from subprocess import Popen
 import pgrep as pgrep
+import customtkinter
+import tkinter
+from tkinter import filedialog
+
+customtkinter.set_appearance_mode("dark")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
 
 
 # File to save the log
@@ -10,6 +18,7 @@ log_file = "./top_process.csv"
 
 # Maintenance file path
 maintenance_file = "/tmp/maintenance.txt"
+
 
 # Loop for 5 minutes with 5 seconds interval
 end_time = time.time() + 300
@@ -43,5 +52,38 @@ while time.time() < end_time:
                 maintenance_entries.append(row)
     print(f"Number of maintenance entries: {len(maintenance_entries)}")
 
-
     time.sleep(5)
+
+entries = []
+
+
+# Show the entries in a GUI
+def show_entries():
+    for entry in entries:
+        print(entry)
+
+
+def save_entries():
+    file_path = filedialog.asksaveasfilename(defaultextension=".csv")
+    with open(file_path, "w", newline="") as f:
+        writing = csv.writer(f)
+        writing.writerow(header)
+        writing.writerows(entries)
+
+
+with open(log_file) as f:
+    reader = csv.reader(f)
+    header = next(reader)  # skip the header
+    for row in reader:
+        if row[1] == "Top was started":
+            entries.append(row)
+
+
+show_button = tkinter.Button(master=root, text="Show entries", command=show_entries)
+show_button.pack(pady=12, padx=10)
+
+save_button = tkinter.Button(master=root, text="Save entries", command=save_entries)
+save_button.pack(pady=12, padx=10)
+
+
+root.mainloop()
